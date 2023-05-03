dc115c9b1129ea3c35a8728ba0c67be66c73fa92
AssaultMachete <69009746+AssaultMachete@users.noreply.github.com>
2023-05-03 11:49:44 +0300

    Js_Commit

---
+++ Js/script.js
@@ -0,0 +1,6 @@
+console.log((100).toString());
+console.log((100.578999).toFixed(2));
+console.log(parseFloat("200.5679"));
+console.log(Number.isInteger(100)); //True
+console.log(Number.isInteger(100.504)); //False
+console.log(Number.isNaN("Ahmad" / 150)); //True
